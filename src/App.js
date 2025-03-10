5f96f7856e5cffe9c32ae76006e238ee8bc2c191
Ravin Kumar <16964978+mr-ravin@users.noreply.github.com>
2025-03-10 23:41:03 +0530

    added text instead of image containing text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-import image_1 from './images/poem_1.png';
-import image_2 from './images/poem_2.png';
-import image_3 from './images/poem_3.png';
-import image_4 from './images/poem_4.png';
 import './App.css';
 
 function App() {
@@ -12,16 +8,76 @@ function App() {
      <br/>
       <table align="center" cellspacing="5" >
       <tr>
-      <td ><img src={image_1} alt="image" style={{ width: '100%', height: 'auto', maxWidth: '100%', display: 'block'}}/></td>
+      <td align="justify">
+      <b>Not A Play</b><br/>
+      by <a href="https://mr-ravin.github.io" target="__blank__">Ravin Kumar</a>
+      <p>
+      Being a being is not a play,<br/>
+      to all the beings shall I say.<br/>
+      Things we do merely matter,<br/>
+      when compared to the world's setter.<br/>
+      Don't just play the part O fellow being,<br/>
+      when you can choose and can think.<br/>
+      Being a being is not a play,<br/>
+      for those who think and then say.
+      </p>
+      &copy; 2021 Ravin Kumar
+      <br/><br/><br/>
+      </td>
       </tr>
       <tr>
-      <td ><img src={image_2} alt="image" style={{ width: '100%', height: 'auto', maxWidth: '100%', display: 'block'}}/></td>
+      <td align="justify">
+      <b>To be</b><br/>
+      by <a href="https://mr-ravin.github.io" target="__blank__">Ravin Kumar</a>
+      <p>
+      To be the spell caster,<br/>
+      to be the new master.<br/>
+      Don't do the work hard,<br/>
+      do work that is harder.<br/>
+      Skilled and smarter,<br/>
+      says, crafter turned master.<br/>
+      Don't do the work hard,<br/>
+      do work that is harder.
+      </p>
+      &copy; 2021 Ravin Kumar
+      <br/><br/><br/>
+      </td>
       </tr>
       <tr>
-      <td><img src={image_3} alt="image" style={{ width: '100%', height: 'auto', maxWidth: '100%', display: 'block'}}/></td>
+      <td align="justify">
+      <b>The devil's rule</b><br/>
+      by <a href="https://mr-ravin.github.io" target="__blank__">Ravin Kumar</a>
+      <p>
+      Honest ones are call fool,<br/>
+      Often happens when the devil rule.<br/>
+      Unemployment and, people cry,<br/>
+      honest ones are thinking why.<br/>
+      News speakers start spreading the lie,<br/>
+      Even the Gods do then say goodbye.<br/>
+      People waiting for the honest's rule,<br/>
+      Often happens when the devil rule.
+      </p>
+      &copy; 2021 Ravin Kumar
+      <br/><br/><br/>
+      </td>
       </tr>
       <tr>
-      <td><img src={image_4} alt="image" style={{ width: '100%', height: 'auto', maxWidth: '100%', display: 'block'}}/></td>
+      <td align="justify">
+      <b>A Journey</b><br/>
+      by <a href="https://mr-ravin.github.io" target="__blank__">Ravin Kumar</a>
+      <p>
+      With every new sun,<br/>
+      a journey does begin.<br/>
+      Offers happiness or cry,<br/>
+      Seems doesn't worth a try.<br/>
+      With each sinking sun,<br/>
+      a searching does begin.<br/>
+      One looking for a bed,<br/>
+      Becoming the dead.
+      </p>
+      &copy; 2021 Ravin Kumar
+      <br/><br/><br/>
+      </td>
       </tr>
       </table>
     </div>
